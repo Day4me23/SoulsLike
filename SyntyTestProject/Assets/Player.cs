cd6faed842096474042570215e6fa903ee073ca5
djw191 <50028101+djw191@users.noreply.github.com>
2022-03-27 17:52:18 -0500

    Player script init

---
+++ SyntyTestProject/Assets/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public void RightLightAttack(InputAction.CallbackContext context) {
+        //Attack Actions
+    }
+    public void RightHeavyAttack(InputAction.CallbackContext context) {
+        //Attack Actions
+    }
+    public void Move(InputAction.CallbackContext context) {
+        //Move Actions
+    }
+    public void Look(InputAction.CallbackContext context) {
+        //Look Actions
+    }
+    public void Use(InputAction.CallbackContext context) {
+        //Use
+    }
+    public void Interact(InputAction.CallbackContext context) {
+        //Interact
+    }
+    public void RollDashJump(InputAction.CallbackContext context) {
+        //Roll/Dash/Jump
+    }
+    public void BlockLeftLightAttack(InputAction.CallbackContext context) {
+        //Block or Light Attack
+    }
+    public void ParryLeftHeavyAttack(InputAction.CallbackContext context) {
+        //Parry or Heavy Left
+    }
+    public void Menu(InputAction.CallbackContext context) {
+        //Open Menu
+    }
+}
