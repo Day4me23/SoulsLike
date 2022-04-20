ecf5bd2ecf112be176e8212ffc9158b04cbee8cc
Day4me23 <Dayneruns5k@gmail.com>
2022-04-19 23:26:16 -0500

    EnemySpawns Floor 1

--- SyntyTestProject/Assets/Scripts/Enemy/EnemySpawner.cs
+++ SyntyTestProject/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -6,16 +6,17 @@ public class EnemySpawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
     public GameObject enemyPrefab;
+    public List<GameObject> enemiesOnFloor = new List<GameObject>();
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
             for (int i = 0; i < spawnPoint.Length; i++)
             {
-                Instantiate(enemyPrefab, spawnPoint[i].transform.position, Quaternion.identity);
+                enemiesOnFloor.Add(Instantiate(enemyPrefab, spawnPoint[i].transform.position, Quaternion.identity));
+
             }
             Debug.Log("Spawn Enemy");
-            //FindObjectOfType<AudioManager>().Play(sound);
             Destroy(this.gameObject);
         }
     }
